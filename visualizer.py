2c23e8065ef1aeed22bc8eac09d0b7b21f385141
Atreya Mitra <ridernealcoc@gmail.com>
2026-02-08 13:56:56 +0530

    Update visualizer.py
    Implemented selection sort algorithm

--- visualizer.py
+++ visualizer.py
@@ -9,7 +9,15 @@ def bubble_sort(arr):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 yield arr
-
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        yield arr
 def visualize_sort():
     arr = [random.randint(1, 100) for _ in range(20)]
     generator = bubble_sort(arr)
